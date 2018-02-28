66ad07e03fff633b2e48fafd58ac79b6ee6001b1
Garg <nitin.garg@centurylink.com>
2018-02-27 23:07:53 -0700

    first commit

---
+++ src/main/java/com/nitin/springboot/cloud/sleuthserver/sleuthserver/SleuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.nitin.springboot.cloud.sleuthserver.sleuthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthServerApplication.class, args);
+	}
+}
